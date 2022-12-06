f61f41c8b4a64e43bee175d7262b6377ea55725d
robport <26696668+robport@users.noreply.github.com>
2022-12-06 23:40:25 +0000

    switch

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import styled from 'styled-components';
+import { Navbar, Container } from 'react-bootstrap';
 
 const CentreLayoutContainer = styled.div`
   padding: 20px;
@@ -11,13 +12,14 @@ export interface Props {
   children: ReactNode;
 }
 
-export const Main = ({children}: Props) => {
+export const Main = ({ children }: Props) => {
   return (
     <div>
+
       <nav className="navbar navbar-light pb-1 pb-sm-3 mb-2 mb-sm-4 border-bottom">
         <div className="container-fluid">
           <div className="navbar-brand">
-            <span className="fs-4">Crypto Registry</span>
+            <a href="/" className="fs-4">Crypto Registry</a>
           </div>
           <div className="navbar-expand" id="navbarNav">
             <ul className="navbar-nav">
@@ -25,6 +27,16 @@ export const Main = ({children}: Props) => {
           </div>
         </div>
       </nav>
+
+
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand className="fs-4" href="/">Crypto Registry</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          </Navbar.Collapse>
+         </Container>
+      </Navbar>
 
       <CentreLayoutContainer>
         {children}
