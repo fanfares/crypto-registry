541c1eb370396fb1e967bc2469d5008ef4f88208
robport <26696668+robport@users.noreply.github.com>
2022-12-16 18:59:26 +0000

    Integration BIP84.

---
+++ client/src/open-api/models/CreateSubmissionDto.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { CustomerHoldingDto } from './CustomerHoldingDto';
+
+export type CreateSubmissionDto = {
+    exchangeZpub: string;
+    exchangeName: string;
+    customerHoldings: Array<CustomerHoldingDto>;
+};
