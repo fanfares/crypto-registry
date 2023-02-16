bab148fe50a8e7ca0b69e663cf71c742f2bbd64c
Rob Porter <rob@flexearn.com>
2023-02-16 12:22:51 +0000

    lint

--- api/src/crypto/is-valid-zpub.spec.ts
+++ api/src/crypto/is-valid-zpub.spec.ts
@@ -1,7 +1,6 @@
 import { isValidZpub } from './is-valid-zpub';
 import { exchangeMnemonic } from './exchange-mnemonic';
 import { Bip84Account } from './bip84-account';
-import { Network } from '@bcr/types';
 
 describe('is-valid-zpub', function() {
   test('valid', () => {
