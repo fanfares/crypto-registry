8653c80c7883d42c4a59e696c326f6b4f89906fe
robport <26696668+robport@users.noreply.github.com>
2023-09-07 11:00:55 +0100

    Reformated Sign In

--- client/src/components/user/sign-in-page.tsx
+++ client/src/components/user/sign-in-page.tsx
@@ -1,22 +1,42 @@
 import { Tab, Tabs } from 'react-bootstrap';
 import { SignIn } from './sign-in';
 import { SignUp } from './sign-up';
+import { Properties } from 'csstype';
+
+const centreContainer: Properties = {
+  display: 'flex',
+  justifyContent: 'center', /* Center horizontally */
+  alignItems: 'center',    /* Center vertically */
+  height: '50vh'           /* 100% of the viewport height */
+}
+
+const loginDialog: Properties = {
+  width: '500px',
+  padding: '20px',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+}
 
 export const SignInPage = () => {
   return (
-    <Tabs
-      defaultActiveKey="sign-in"
-      className="mb-3 h-100"
-      fill>
+    <div style={centreContainer}>
+      <div style={loginDialog}>
+        <Tabs
+          defaultActiveKey="sign-in"
+          className="mb-3 h-100"
+          fill>
 
-      <Tab eventKey="sign-in" title="Sign In">
-        <SignIn />
-      </Tab>
+          <Tab eventKey="sign-in" title="Sign In">
+            <SignIn/>
+          </Tab>
 
-      <Tab eventKey="sign-up" title="Sign Up">
-        <SignUp />
-      </Tab>
+          <Tab eventKey="sign-up" title="Sign Up">
+            <SignUp/>
+          </Tab>
 
-    </Tabs>
+        </Tabs>
+      </div>
+    </div>
+
+
   );
 }
