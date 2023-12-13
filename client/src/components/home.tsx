2caa8a6d31f0f05995c59820c7914716b3e35cd5
robport <26696668+robport@users.noreply.github.com>
2023-12-13 18:18:40 +0000

    Signed Version.

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -11,7 +11,7 @@ export const Home = () => {
 
   return (
     <CentreLayoutContainer>
-      <h1>Customer Deposits Registry (Signed Version)</h1>
+      <h1>Customer Deposits Registry</h1>
 
       <p>Welcome to the home of Crypto Compliance. This network allows custodians of
         Bitcoin to prove they are holding all of their customers assets </p>
