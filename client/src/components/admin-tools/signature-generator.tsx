7323537c96ca8febd3c1d4398f564623c9e363b0
robport <26696668+robport@users.noreply.github.com>
2024-03-08 12:41:22 +0000

    Signature Generator Working.

--- client/src/components/admin-tools/signature-generator.tsx
+++ client/src/components/admin-tools/signature-generator.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import { FormProvider, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useStore } from '../../store';
 import ButtonPanel from '../utils/button-panel';
 import BigButton from '../utils/big-button.tsx';
@@ -49,7 +49,6 @@ const SignatureGenerator = () => {
   const [network, setNetwork] = useState<Network | null>(null);
   const [error, setError] = useState<string>('');
   const [result, setResult] = useState<SignatureGeneratorResultDto | null>(null);
-  const methods = useForm();
 
   const handleSubmission = async (data: Inputs) => {
     setLocalIsWorking(true);
@@ -71,7 +70,7 @@ const SignatureGenerator = () => {
   };
 
   const updateBlockForSignatureMessage = async () => {
-    const latestBlockHash = await BitcoinService.getLatestBlock(Network.TESTNET)
+    const latestBlockHash = await BitcoinService.getLatestBlock(Network.TESTNET);
     setValue('message', latestBlockHash.hash);
   };
 
@@ -79,150 +78,148 @@ const SignatureGenerator = () => {
     <>
       <h1>Signature Generator</h1>
       <p>Use this utility to generate the signature for an address.</p>
-      <FormProvider {...methods}>
-        <Form onSubmit={handleSubmit(handleSubmission)}>
+      <Form onSubmit={handleSubmit(handleSubmission)}>
 
-          <div style={{marginBottom: 30, display: 'flex', flexDirection: 'column'}}>
-            <FloatingLabel label="Exchange Private Key">
-              <Form.Control
-                style={{maxWidth: '1000px'}}
-                type="text"
-                isInvalid={!!errors?.privateKey}
-                placeholder="Extended Private Key (zpub)"
-                {...register('privateKey', {
-                  required: 'Private Key is required',
-                  validate: async zpub => {
-                    setError('');
-                    const result = await validateExtendedKey(zpub);
-                    if (result.valid) {
-                      if (!result.isPrivate) {
-                        return 'This is not a private key';
-                      }
-                      setNetwork(result.network ?? null);
-                      return true;
-                    } else {
-                      setNetwork(null);
-                      return 'Invalid Private key';
+        <div style={{marginBottom: 30, display: 'flex', flexDirection: 'column'}}>
+          <FloatingLabel label="Exchange Private Key">
+            <Form.Control
+              style={{maxWidth: '1000px'}}
+              type="text"
+              isInvalid={!!errors?.privateKey}
+              placeholder="Extended Private Key (zpub)"
+              {...register('privateKey', {
+                required: 'Private Key is required',
+                validate: async zpub => {
+                  setError('');
+                  const result = await validateExtendedKey(zpub);
+                  if (result.valid) {
+                    if (!result.isPrivate) {
+                      return 'This is not a private key';
                     }
+                    setNetwork(result.network ?? null);
+                    return true;
+                  } else {
+                    setNetwork(null);
+                    return 'Invalid Private key';
                   }
-                })} />
-            </FloatingLabel>
-          </div>
+                }
+              })} />
+          </FloatingLabel>
+        </div>
 
-          <div style={{marginBottom: 30, display: 'flex', flexDirection: 'column'}}>
-            <FloatingLabel label="Max Index">
-              <Form.Control
-                style={{maxWidth: '1000px'}}
-                type="number"
-                isInvalid={!!errors?.maxIndex}
-                placeholder="Max Index"
-                {...register('maxIndex')}/>
+        <div style={{marginBottom: 30, display: 'flex', flexDirection: 'column'}}>
+          <FloatingLabel label="Max Index">
+            <Form.Control
+              style={{maxWidth: '1000px'}}
+              type="number"
+              isInvalid={!!errors?.maxIndex}
+              placeholder="Max Index"
+              {...register('maxIndex')}/>
 
-              <Form.Text className="text-muted">
-                Max Index used to search for address within wallet.
-              </Form.Text>
-            </FloatingLabel>
-
-            <Form.Text className="text-danger">
-              <ErrorMessage errors={errors} name="maxIndex"/>
+            <Form.Text className="text-muted">
+              Max Index used to search for address within wallet.
             </Form.Text>
+          </FloatingLabel>
 
-          </div>
+          <Form.Text className="text-danger">
+            <ErrorMessage errors={errors} name="maxIndex"/>
+          </Form.Text>
 
-          <div style={{marginBottom: 30}}>
-            <FloatingLabel label="Address">
-              <Form.Control
-                style={{maxWidth: '1000px'}}
-                isInvalid={!!errors?.address}
-                placeholder="Address"
-                {...register('address', {
-                  required: 'Address is required'
-                })} />
+        </div>
 
-              <Form.Text className="text-muted">
-                Address containing exchange funds
-              </Form.Text>
-            </FloatingLabel>
-            <Form.Text className="text-danger">
-              <ErrorMessage errors={errors} name="address"/>
+        <div style={{marginBottom: 30}}>
+          <FloatingLabel label="Address">
+            <Form.Control
+              style={{maxWidth: '1000px'}}
+              isInvalid={!!errors?.address}
+              placeholder="Address"
+              {...register('address', {
+                required: 'Address is required'
+              })} />
+
+            <Form.Text className="text-muted">
+              Address containing exchange funds
             </Form.Text>
-          </div>
+          </FloatingLabel>
+          <Form.Text className="text-danger">
+            <ErrorMessage errors={errors} name="address"/>
+          </Form.Text>
+        </div>
 
-          <div style={{marginBottom: 30}}>
-            <InputWithUpdateButton
-              updateFn={() => updateBlockForSignatureMessage()}
-              register={register('message', {
-                required: 'Message is required'
-              })}
-              subtext="Block to use for signature message"
-              label="Signature Block"/>
-          </div>
+        <div style={{marginBottom: 30}}>
+          <InputWithUpdateButton
+            updateFn={() => updateBlockForSignatureMessage()}
+            register={register('message', {
+              required: 'Message is required'
+            })}
+            subtext="Block to use for signature message"
+            label="Signature Block"/>
+        </div>
 
-          {network ?
-            <FloatingLabel
-              style={{marginBottom: 30}}
-              label="Network">
-              <Input type="text"
-                     disabled={true}
-                     value={network}/>
-              <Form.Text className="text-muted">
-                The network for this Submission.
-              </Form.Text>
-            </FloatingLabel>
-            : null}
+        {network ?
+          <FloatingLabel
+            style={{marginBottom: 30}}
+            label="Network">
+            <Input type="text"
+                   disabled={true}
+                   value={network}/>
+            <Form.Text className="text-muted">
+              The network for this Submission.
+            </Form.Text>
+          </FloatingLabel>
+          : null}
 
-          {result ? <>
-            <FloatingLabel
-              style={{marginBottom: 30}}
-              label="Derivation Path">
-              <Input type="text"
-                     disabled={true}
-                     value={result.derivationPath}/>
-              <Form.Text className="text-muted">
-                Derivation path of address in wallet of index {result.index}, which is
-                a {result.change ? 'change address' : 'receiving address'}
-              </Form.Text>
-            </FloatingLabel>
+        {result ? <>
+          <FloatingLabel
+            style={{marginBottom: 30}}
+            label="Derivation Path">
+            <Input type="text"
+                   disabled={true}
+                   value={result.derivationPath}/>
+            <Form.Text className="text-muted">
+              Derivation path of address in wallet of index {result.index}, which is
+              a {result.change ? 'change address' : 'receiving address'}
+            </Form.Text>
+          </FloatingLabel>
 
-            <FloatingLabel
-              style={{marginBottom: 30}}
-              label="Signature">
-              <Input type="text"
-                     disabled={true}
-                     value={result.signature}/>
-            </FloatingLabel>
+          <FloatingLabel
+            style={{marginBottom: 30}}
+            label="Signature">
+            <Input type="text"
+                   disabled={true}
+                   value={result.signature}/>
+          </FloatingLabel>
 
-            <FloatingLabel
-              style={{marginBottom: 30}}
-              label="Balance">
-              <Input type="text"
-                     disabled={true}
-                     value={formatSatoshi(result.balance)}/>
-            </FloatingLabel>
+          <FloatingLabel
+            style={{marginBottom: 30}}
+            label="Balance">
+            <Input type="text"
+                   disabled={true}
+                   value={formatSatoshi(result.balance)}/>
+          </FloatingLabel>
 
-            <FloatingLabel
-              style={{marginBottom: 30}}
-              label="Valid From">
-              <Input type="text"
-                     disabled={true}
-                     value={formatDate(result.validFromDate)}/>
-            </FloatingLabel>
+          <FloatingLabel
+            style={{marginBottom: 30}}
+            label="Valid From">
+            <Input type="text"
+                   disabled={true}
+                   value={formatDate(result.validFromDate)}/>
+          </FloatingLabel>
 
-          </> : null
-          }
+        </> : null
+        }
 
-          <div>
-            <MyErrorMessage errorMessage={error}/>
-            <ButtonPanel>
-              <BigButton disabled={!isValid}
-                         loading={isWorking || localIsWorking}
-                         htmlType="submit">
-                {localIsWorking ? 'Generating...' : 'Generate'}
-              </BigButton>
-            </ButtonPanel>
-          </div>
-        </Form>
-      </FormProvider>
+        <div>
+          <MyErrorMessage errorMessage={error}/>
+          <ButtonPanel>
+            <BigButton disabled={!isValid}
+                       loading={isWorking || localIsWorking}
+                       htmlType="submit">
+              {localIsWorking ? 'Generating...' : 'Generate'}
+            </BigButton>
+          </ButtonPanel>
+        </div>
+      </Form>
     </>
   );
 };
