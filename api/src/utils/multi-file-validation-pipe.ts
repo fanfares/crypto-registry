50c656eb9231dc78191653e7176a8ca36fa2e620
robport <26696668+robport@users.noreply.github.com>
2024-03-10 09:43:02 +0000

    Changed file validation.

--- api/src/utils/multi-file-validation-pipe.ts
+++ api/src/utils/multi-file-validation-pipe.ts
@@ -4,12 +4,8 @@ export class MultiFileValidationPipe implements PipeTransform {
   transform(files: { [fieldname: string]: Express.Multer.File }) {
     for (const field in files) {
       const file = files[field][0];
-      if (file.size > 10000) {
+      if (file.size > 100000) {
         throw new BadRequestException('File size is too large:' + file.size + ' (max 10000)');
-      }
-      if (file.mimetype !== 'text/csv') {
-        // todo - on firefox, file type is xl/csv
-        throw new BadRequestException('Invalid file type:' + file.mimetype );
       }
     }
     return files;
